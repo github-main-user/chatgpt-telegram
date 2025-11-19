b6a4b2a106e0ef33dfd8e6282e245a85823a2d97
github-main-user <kolya.skripn@mail.ru>
2025-11-19 17:35:13 +0300

    feat: add a button to clear dialog

--- src/handlers.py
+++ src/handlers.py
@@ -1,16 +1,25 @@
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
 
 from src.llm import answer_message, clear_dialog
 
 router = Router()
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton(text="Новый запрос")]], resize_keyboard=True
+)
 
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
     clear_dialog(message.from_user.id)  # type: ignore
-    await message.answer("UwU")
+    await message.answer("UwU", reply_markup=keyboard)
+
+
+@router.message(F.text == "Новый запрос")
+async def cmd_clear_dialog(message: Message):
+    clear_dialog(message.from_user.id)  # type: ignore
+    await message.answer("Контекст диалога сброшен", reply_markup=keyboard)
 
 
 @router.message(Command("help"))
@@ -25,5 +34,6 @@ async def cmd_answer_user(message: Message):
 
     answer = await answer_message(message.from_user.id, message.text)  # type: ignore
     await message.answer(
-        answer if answer is not None else "Model provided an empty answer"
+        answer if answer is not None else "Model provided an empty answer",
+        reply_markup=keyboard,
     )
